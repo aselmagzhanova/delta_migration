a83e260a8e1128287f6dcb5f4467f1217e213c54
aselmagzhanova <asik_asek@list.ru>
2024-09-19 13:56:41 +0300

    v1

--- main.py
+++ main.py
@@ -82,17 +82,75 @@ if __name__ == '__main__':
             print(row)
             if not row:
                 break
-            content_uid = row[0].replace("-", "").upper()
-            create_date = row[1]
+            content_uid = row[8].replace("-", "").upper()
+            create_date = row[9]
             year_str = create_date.strftime("%Y")
             yyyymmdd_str = create_date.strftime("%Y%m%d")
             two_digits = content_uid[:2].upper()
-            context = row[27]
+            context = row[1]
             bucket_name = str("filestore-" + context + "-" + year_str)
             object_key = f"{yyyymmdd_str}/{two_digits}/{content_uid}"
-            response = minio_client.get_object(bucket_name, object_key)
-            file_content = response.read()
+            file_content = minio_client.get_object(bucket_name, object_key).read()
+            lob = ora_cursor.var(cx_Oracle.BLOB)
+            lob.setvalue(0, file_content)
             #print(file_content)
+            file_uid_ins = row[0].replace("-", "").upper()
+            context_ins = row[1]
+            file_name_ins = row[2]
+            state_ins = row[3]
+            state_after_handlers_ins = row[4]
+            create_date_fm_ins = row[5]
+            update_date_fm_ins = row[6]
+            content_location_ins = row[7]
+            content_uid_ins = row[8].replace("-", "").upper()
+            create_date_ins = row[9]
+            upload_date_ins = row[10]
+            mime_type_id_ins = row[11]
+            upload_status_ins = row[12]
+            file_size_ins = row[13]
+            sign_gost34_ins = row[14]
+            sign_sha256_ins = row[15]
+            crypto_date_ins = row[16]
+            crypto_status_ins = row[17]
+            crypto_message_ins = row[18]
+            av_check_date_ins = row[19]
+            av_check_status_ins = row[20]
+            av_check_message_ins = row[21]
+            converted_content_uid_ins = row[22]
+            if converted_content_uid_ins is not None:
+                converted_content_uid_ins = converted_content_uid_ins.replace("-", "").upper()
+            convert_date_ins = row[23]
+            convert_status_ins = row[24]
+            convert_message_ins = row[25]
+            macro_check_date_ins = row[26]
+            macro_check_status_ins = row[27]
+            macro_check_message_ins = row[28]
+            forbidden_check_date_ins = row[29]
+            forbidden_check_status_ins = row[30]
+            forbidden_check_result_ins = row[31]
+            forbidden_check_message_ins = row[32]
+            container_name_ins = row[33]
+            ora_cursor.execute(sql_scripts.ora_insert, file_uid=file_uid_ins, context=context_ins,
+                               file_name=file_name_ins, state=state_ins,
+                               state_after_handlers=state_after_handlers_ins,
+                               create_date_fm=create_date_fm_ins, update_date_fm=update_date_fm_ins,
+                               content_location=content_location_ins, content_uid=content_uid_ins,
+                               create_date=create_date_ins, upload_date=upload_date_ins,
+                               mime_type_id=mime_type_id_ins, upload_status=upload_status_ins,
+                               file_size=file_size_ins, sign_gost34=sign_gost34_ins, sign_sha256=sign_sha256_ins,
+                               crypto_date=crypto_date_ins, crypto_status=crypto_status_ins,
+                               crypto_message=crypto_message_ins, av_check_date=av_check_date_ins,
+                               av_check_status=av_check_status_ins, av_check_message=av_check_message_ins,
+                               converted_content_uid=converted_content_uid_ins, convert_date=convert_date_ins,
+                               convert_status=convert_status_ins, convert_message=convert_message_ins,
+                               macro_check_date=macro_check_date_ins, macro_check_status=macro_check_status_ins,
+                               macro_check_message=macro_check_message_ins,
+                               forbidden_check_date=forbidden_check_date_ins,
+                               forbidden_check_status=forbidden_check_status_ins,
+                               forbidden_check_result=forbidden_check_result_ins,
+                               forbidden_check_message=forbidden_check_message_ins,
+                               container_name=container_name_ins, file_content=lob)
+            pg_conn.cursor().execute(sql_scripts.pg_upd, (row[0],))
 
 
     except (Exception, cx_Oracle.Error, psycopg2.Error) as error:
--- sql_scripts.py
+++ sql_scripts.py
@@ -1,6 +1,14 @@
 #test
 pg_get_data = u"""
-select cm.content_uid,
+select fm.file_uid,
+    fm.context,
+    fm.file_name,
+    fm.state,
+    fm.state_after_handlers,
+    fm.create_date,
+    fm.update_date,
+    fm.content_location,
+    cm.content_uid,
     cm.create_date,
     cm.upload_date,
     cm.mime_type_id,
@@ -25,35 +33,33 @@ select cm.content_uid,
     cm.forbidden_check_status,
     cm.forbidden_check_result,
     cm.forbidden_check_message,
-    cm.container_name,
-    fm.file_uid,
-    fm.context,
-    fm.file_name,
-    fm.state,
-    fm.state_after_handlers,
-    fm.create_date,
-    fm.update_date,
-    fm.content_location
+    cm.container_name
 from fs_main.content_metadata cm
 join fs_main.file_metadata fm 
 on cm.content_uid = fm.content_uid
 where cm.create_date >= %s
-and cm.is_migrated is null;
+and not fm.is_migrated;
 """
 
 ora_insert = """
-insert into FS_MAIN.FILE_AND_CONTENT_METADATA (CONTENT_UID, CREATE_DATE, UPLOAD_DATE,
+insert into FS_MAIN.FILE_AND_CONTENT_METADATA (FILE_UID, CONTEXT, FILE_NAME, STATE, STATE_AFTER_HANDLERS, CREATE_DATE_FM, UPDATE_DATE_FM,
+CONTENT_LOCATION, CONTENT_UID, CREATE_DATE, UPLOAD_DATE,
 MIME_TYPE_ID, UPLOAD_STATUS, FILE_SIZE, SIGN_GOST34, SIGN_SHA256, CRYPTO_DATE, CRYPTO_STATUS,
 CRYPTO_MESSAGE, AV_CHECK_DATE, AV_CHECK_STATUS, AV_CHECK_MESSAGE, CONVERTED_CONTENT_UID,
 CONVERT_DATE, CONVERT_STATUS, CONVERT_MESSAGE, MACRO_CHECK_DATE, MACRO_CHECK_STATUS,
 MACRO_CHECK_MESSAGE, FORBIDDEN_CHECK_DATE, FORBIDDEN_CHECK_STATUS, FORBIDDEN_CHECK_RESULT, FORBIDDEN_CHECK_MESSAGE,
-CONTAINER_NAME, FILE_UID, CONTEXT, FILE_NAME, STATE, STATE_AFTER_HANDLERS, CREATE_DATE_FM, UPDATE_DATE_FM,
-CONTENT_LOCATION, FILE_CONTENT)
-values (:CONTENT_UID, :CREATE_DATE, :UPLOAD_DATE,
-:MIME_TYPE_ID, :UPLOAD_STATUS, :FILE_SIZE, :SIGN_GOST34, :SIGN_SHA256, :CRYPTO_DATE, :CRYPTO_STATUS,
-:CRYPTO_MESSAGE, :AV_CHECK_DATE, :AV_CHECK_STATUS, :AV_CHECK_MESSAGE, :CONVERTED_CONTENT_UID,
-:CONVERT_DATE, :CONVERT_STATUS, :CONVERT_MESSAGE, :MACRO_CHECK_DATE, :MACRO_CHECK_STATUS,
-:MACRO_CHECK_MESSAGE, :FORBIDDEN_CHECK_DATE, :FORBIDDEN_CHECK_STATUS, :FORBIDDEN_CHECK_RESULT, :FORBIDDEN_CHECK_MESSAGE,
-:CONTAINER_NAME, :FILE_UID, :CONTEXT, :FILE_NAME, :STATE, :STATE_AFTER_HANDLERS, :CREATE_DATE_FM, :UPDATE_DATE_FM,
-:CONTENT_LOCATION, :FILE_CONTENT)
+CONTAINER_NAME, FILE_CONTENT)
+values (:file_uid, :context, :file_name, :state, :state_after_handlers, :create_date_fm, :update_date_fm,
+:content_location, :content_uid, :create_date, :upload_date,
+:mime_type_id, :upload_status, :file_size, :sign_gost34, :sign_sha256, :crypto_date, :crypto_status,
+:crypto_message, :av_check_date, :av_check_status, :av_check_message, :converted_content_uid,
+:convert_date, :convert_status, :convert_message, :macro_check_date, :macro_check_status,
+:macro_check_message, :forbidden_check_date, :forbidden_check_status, :forbidden_check_result, :forbidden_check_message,
+:container_name, :file_content)
+"""
+
+pg_upd = """
+update fs_main.file_metadata
+set is_migrated = true
+where file_uid = %s
 """
